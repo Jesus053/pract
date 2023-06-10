033f158e4326374aca7a82e6750940c4e09b5145
Jesus053 <marquezjesus0558@gmail.com>
2023-06-09 23:01:25 -0600

    ejerc5

---
+++ Formula_General_Ecuacuinescuadraticas.js
@@ -0,0 +1,16 @@
+function FormulaGeneral(a, b, c) {
+ 
+  let discriminante = (b ** 2 - 4 * a * c);
+  if (discriminante > 0){
+    let x1 = -b + Math.sqrt(discriminante)/ (2*a);
+    let x2 = -b - Math.sqrt(discriminante)/ (2*a);
+    return "Dos soluciones" + x1 + " y "+ x2;
+  }
+    else if(discriminante == 0){
+      let x1 =  -b / (2*a);
+      return console.log("Una solucion doble x1=" +x1, "x2="+x1 )
+    }
+    else if (discriminante < 0){
+      return "Sin solución"
+    }    
+}
