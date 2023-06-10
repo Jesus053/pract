48cdfb355ea10fee507951430ddb445bdfddca8a
Jesus053 <marquezjesus0558@gmail.com>
2023-06-09 22:58:46 -0600

    ejercicio3

---
+++ Objetos.js
@@ -0,0 +1,18 @@
+const DaTos = {
+    "Nombre": "Juan",
+    "Profecion": "Licenciado en conputacion",
+    "ubicacion": "Oaxaca"
+  };
+  
+  DaTos["Nombre"];
+  DaTos['Profecion'];
+  DaTos["ubicacion"];   
+
+  <p class="clase-html">
+  <span>JSON es un formato de texto que forma parte del sistema de JavaScript y que se deriva de su sintaxis,
+     pero no tiene como objetivo la creación de programas, sino el acceso, almacenamiento e intercambio de datos.
+     Usualmente es conocido como una alternativa al lenguaje XML. La sintaxis de JSON funciona de modo similar a
+     JavaScript. Es un documento digital creado en este lenguaje que almacena información organizada,con el 
+     fin de hacer más simple su búsqueda y acceso. La ventaja de este formato es que permite obtener código 
+     legible para las personas con nombres y valores que funcionan como indicadores de la información que contienen</span>
+</p>
